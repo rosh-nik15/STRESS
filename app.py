cba3d04118fd7056c57507a4f53cb861c1cecc69
Roshni Kumari <155992598+rosh-nik15@users.noreply.github.com>
2026-01-13 10:35:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 app = Flask(__name__)
 
 # Load model
-with open("model/xgb_model.pkl", "rb") as f:
+with open("xgb_model.pkl", "rb") as f:
     model = pickle.load(f)
 
 @app.route("/")
